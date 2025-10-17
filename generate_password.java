4b6e3be5ab08eef06a2167c8ccbecff3362157c1
STARTER <vitaliivv2016@gmail.com>
2025-10-17 23:06:30 +0300

    Создали генератор паролей на Java

--- generate_password.java
+++ generate_password.java
@@ -2,13 +2,10 @@ import java.util.Random;
 
 public class PasswordGenerator {
     
-    public static String generatePassword(int length) {
-        // Все возможные символы для пароля
-        String characters = "abcdefghijklmnopqrstuvwxyz";
+    public static String generatePassword(int length) {String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Random random = new Random();
         StringBuilder password = new StringBuilder();
         
-        // Генерируем пароль заданной длины
         for (int i = 0; i < length; i++) {
             int index = random.nextInt(characters.length());
             password.append(characters.charAt(index));
@@ -18,9 +15,8 @@ public class PasswordGenerator {
     }
     
     public static void main(String[] args) {
-        // Генерируем пароль из 11 символов (как в примере)
         int passwordLength = 11;
         String password = generatePassword(passwordLength);
-        System.out.println(password);
+        System.out.println("Сгенерированный пароль: " + password);
     }
 }
