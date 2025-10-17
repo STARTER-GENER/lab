1cdadc1e1de8ed15c0990529a74f875cf8aeeba3
STARTER <vitaliivv2016@gmail.com>
2025-10-17 23:41:21 +0300

    Добавлены цифры и спецсимволы в генератор паролей

--- generate_password.java
+++ generate_password.java
@@ -1,19 +1,20 @@
 import java.util.Random;
 
 public class PasswordGenerator {
-    
-    public static String generatePassword(int length) {String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String generatePassword(int length) {
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
         Random random = new Random();
         StringBuilder password = new StringBuilder();
-        
+
         for (int i = 0; i < length; i++) {
             int index = random.nextInt(characters.length());
             password.append(characters.charAt(index));
         }
-        
+
         return password.toString();
     }
-    
+
     public static void main(String[] args) {
         int passwordLength = 11;
         String password = generatePassword(passwordLength);
